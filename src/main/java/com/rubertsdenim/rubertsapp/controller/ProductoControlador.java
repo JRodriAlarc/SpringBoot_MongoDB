14e0bf54185dca3fcd12d4dd1c6656928e595ebb
JRodriAlarc <jona10131313@gmail.com>
2024-06-20 20:41:30 -0600

    Creacion de la Capa Controlador (Clase ProductoControlador)

---
+++ src/main/java/com/rubertsdenim/rubertsapp/controller/ProductoControlador.java
@@ -0,0 +1,44 @@
+package com.rubertsdenim.rubertsapp.controller;
+
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.rubertsdenim.rubertsapp.model.Producto;
+import com.rubertsdenim.rubertsapp.service.ProductoServicio;
+
+@RestController
+@RequestMapping("/inventario")
+//@Cross0rigin (value = "http://localhost:5500")
+public class ProductoControlador {
+
+    private static final Logger logger = LoggerFactory.getLogger(ProductoControlador.class);
+    
+    @Autowired
+    private ProductoServicio productoServicio;
+
+    /*
+    @PostMapping("/productos")
+    public ResponseEntity<?> saveProducto(@RequestBody Producto producto){
+        try{
+            Producto productoGuardado = productoServicio.guardarProducto(producto);
+            return new ResponseEntity<Productos>(productoGuardado, HttpStatus.CREATED);
+        } catch(Exception e){
+            return new ResponseEntity<String>(e.getCause().toString(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    */
+
+    @GetMapping("/productos")
+    public List<Producto> obteneProductos(){
+        List<Producto> productos = this.productoServicio.listarProductos();
+        logger.info("Productos Obtenidos: ");
+        productos.forEach((producto -> logger.info(producto.toString())));
+        return productos;
+    }
+    
+
+}
