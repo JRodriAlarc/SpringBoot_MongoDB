9b864b3c51124c3ffe2a75bc5f4c8446c4c93296
JRodriAlarc <jona10131313@gmail.com>
2024-06-20 18:14:43 -0600

    CRUD de Inventario usando Mongo DB

---
+++ src/main/java/com/rubertsdenim/rubertsapp/RubertsappApplication.java
@@ -0,0 +1,13 @@
+package com.rubertsdenim.rubertsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RubertsappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RubertsappApplication.class, args);
+	}
+
+}
