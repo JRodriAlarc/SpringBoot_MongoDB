42bc8e32d284cc0dd5c48b3e1e7de4097480db96
JRodriAlarc <jona10131313@gmail.com>
2024-06-26 20:01:52 -0600

    Listar Productos del inventario en la interfaz

--- src/main/java/com/rubertsdenim/rubertsapp/ControladorInicio.java
+++ src/main/java/com/rubertsdenim/rubertsapp/ControladorInicio.java
@@ -1,13 +1,26 @@
 package com.rubertsdenim.rubertsapp;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.rubertsdenim.rubertsapp.model.Producto;
+import com.rubertsdenim.rubertsapp.service.ProductoServicio;
 
 @Controller
 public class ControladorInicio {
 
-    @GetMapping("/")
-    public String inicio(){
+    @Autowired
+    private ProductoServicio productoServicio;
+
+    @GetMapping("/inventario")
+    public String verInventario(Model model){
+        List<Producto> productos = productoServicio.listarProductos();
+        model.addAttribute("productos", productos);
         return "inventario";
     }
+
 }
