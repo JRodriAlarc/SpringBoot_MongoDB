a8e099a3debde5ba96bc9d1a818c657926da676b
JRodriAlarc <jona10131313@gmail.com>
2024-06-28 10:57:48 -0600

    Actualizar/Editar Productos Esxistentes del Inventario desde la interfaz

--- src/main/java/com/rubertsdenim/rubertsapp/ControladorInicio.java
+++ src/main/java/com/rubertsdenim/rubertsapp/ControladorInicio.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.rubertsdenim.rubertsapp.model.Producto;
@@ -36,6 +37,30 @@ public class ControladorInicio {
     public String registrarNuevoProducto(@ModelAttribute("producto") Producto producto) {
         producto.setIdProducto(new ObjectId());
         productoServicio.guardarProducto(producto);
+        return "redirect:/inventario";
+    }
+
+    @GetMapping("/inventario/update/{id}")
+    public String editarProducto(@PathVariable ObjectId id, Model model) {
+        Producto producto = productoServicio.buscarProductoPorId(id);
+        if (producto != null) {
+            model.addAttribute("producto", producto);
+            return "update";
+        } else {
+            return "redirect:/inventario";
+        }
+    }
+
+    @PostMapping("/inventario/update")
+    public String actualizarProducto(@ModelAttribute("producto") Producto producto) {
+        Producto productoExistente = productoServicio.buscarProductoPorId(producto.getIdProducto());
+        if (productoExistente != null) {
+            productoExistente.setNombre(producto.getNombre());
+            productoExistente.setCategoria(producto.getCategoria());
+            productoExistente.setCantidad(producto.getCantidad());
+            productoExistente.setColor(producto.getColor());
+            productoServicio.guardarProducto(productoExistente);
+        }
         return "redirect:/inventario";
     }
 
